dc805a8d0c060ea067f79e8e3005bf7e089c7062
etcarter32 <etcarter@ucsd.edu>
2024-10-01 18:48:53 +0000

    fixed a bug where height could be negative in pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -8,6 +8,12 @@ int main(int argc, char **argv) {
 	}
 
 	size_t height = atoi(argv[1]);
+
+	if (height <= 0) {
+		printf("height must be greater than 0");
+		return 1;
+	}
+
 	for (size_t i = 0; i < height; ++i) {
 		for (size_t j = height - i - 1; j > 0; --j) {
 			printf(" ");
