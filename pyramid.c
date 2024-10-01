913ac6c6a9528285e14e84feae6fc2e782cd95e5
etcarter32 <etcarter@ucsd.edu>
2024-10-01 18:46:53 +0000

    added pyramid.c

---
+++ pyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("Usage: pyramid.c [height]");
+		return 1;
+	}
+
+	size_t height = atoi(argv[1]);
+	for (size_t i = 0; i < height; ++i) {
+		for (size_t j = height - i - 1; j > 0; --j) {
+			printf(" ");
+		}
+		for (size_t j = 0; j < i + 1; ++j) {
+			printf("#");
+		}
+		for (size_t j = 0; j < i + 1; ++j) {
+			printf("#");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
